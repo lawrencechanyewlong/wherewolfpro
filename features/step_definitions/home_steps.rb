d341c65b65a97a92c8a1a6cf9008ce2de5b278a5
sngguojie <sngguojie@gmail.com>
2016-04-01 01:19:54 -0700

    shift to complete

--- features/step_definitions/home_steps.rb
+++ features/step_definitions/home_steps.rb
@@ -1,3 +1,14 @@
 Then /^I should see the image "(.+)"$/ do |image|
     page.should have_xpath("//img[@id=\"#{image}\"]")
 end
+
+And  /^I log in$/ do
+	step "I am on the new user page"
+	step "I fill in \"Email\" with \"user@berkeley.edu\""
+  	step "I fill in \"Password\" with \"password\""
+  	step "I press \"Submit info\""
+	step "I should be on the login page"
+	step "I fill in \"Email\" with \"user@berkeley.edu\""
+	step "I fill in \"Password\" with \"password\""
+	step "I press \"login\""
+end
