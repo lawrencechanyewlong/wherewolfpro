d4643b6e12cfd1a9250349d295092ccc3f61c91f
sngguojie <sngguojie@gmail.com>
2016-04-20 22:27:37 -0700

    hist

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -320,24 +320,25 @@ class EventController < ApplicationController
       @eid = params[:eid]
       @event = Event.where(id: @eid.to_i).first
       logger.debug "event: #{@event.inspect}"
-      @receiver_name = @event[:receiver_name]
-      @receiver = @event[:receiver]
-      @formatted_address = @event[:address_string]
-      @duration_setting = @event[:duration_setting]
-      # @datetime_sent = @event[:datetime_sent]
+      session[:receiver_name] = @event[:receiver_name]
+      session[:receiver] = @event[:receiver]
+      session[:address_string] = @event[:address_string]
+      session[:duration_setting] = @event[:duration_setting]
+      session[:message] = @event[:message]
+      # # @datetime_sent = @event[:datetime_sent]
 
-      # session[:receiver_name] = @receiver_name
-      session[:info] = @receiver
-      session[:formatted_address] = @formatted_address
-      session[:duration] = @duration_setting
-      # session[:datetime_sent] = @datetime_sent
-      session[:message] = ""
+      # # session[:receiver_name] = @receiver_name
+      # session[:info] = @receiver
+      # session[:formatted_address] = @formatted_address
+      # session[:duration] = @duration_setting
+      # # session[:datetime_sent] = @datetime_sent
+      # session[:message] = ""
 
       logger.debug "session[:info]: #{session[:info]}"
       logger.debug "session[:formatted_address]: #{session[:formatted_address]}"
       logger.debug "session[:duration]: #{session[:duration]}"
-      
-      render text: "<script>window.location = '#{event_summary_path}';</script>", status: 200
+      redirect_to event_summary_path
+      # render text: "<script>window.location = '#{event_summary_path}';</script>", status: 200
     end
 
   end
