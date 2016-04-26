1d64279664067b7f0c4e466a309fefc13379fa36
Robert Chang <Cheraws@gmail.com>
2016-04-26 20:36:27 +0000

    added rspec for email

--- spec/ongoing/controllers/event_controller_spec.rb
+++ spec/ongoing/controllers/event_controller_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
-describe EventController do
+describe EventController, :type => :controller do
     
     describe 'new' do
         it 'should go to the select contacts page' do
             # should route_to(:controller => "event", :action => "select_destination")
+        end
+    end
+    describe 'new' do
+        it 'should send an email' do
+            get(:send_mail, {'id' => "12"}, {:url => "http://wherewolf.heroku.com", :message => "cake", :receiver => "asdf@berkeley.edu"})
         end
     end
 end
